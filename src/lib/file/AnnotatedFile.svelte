479af8df87c0e7c106b767adf434ded11f7e7e56
Patrick Smadbeck <patsmad@gmail.com>
2023-06-14 21:17:58 -0400

    add some boxes and stars and stuff

--- src/lib/file/AnnotatedFile.svelte
+++ src/lib/file/AnnotatedFile.svelte
@@ -5,38 +5,52 @@ export let img_src;
 export let selected;
 
 let img;
-let img_x;
-let img_y;
+let original_width;
+let new_width;
 
 function loadImg() {
-    img_x = img.x;
-    img_y = img.y;
+    original_width = img.width;
+    new_width = 1500;
 }
 
-$: console.log(img_x, img_y);
+$: console.log(original_width, img?.x, img?.y, img?.height, img?.width);
 
 </script>
 
 <main>
     <div>
         <img class="annotated-file"
-             bind:this={img}
-             src={img_src}
-             alt="Annotated file for file id {selected}"
-             on:dblclick={(mouse) => console.log(mouse.offsetX, mouse.offsetY, mouse.x, mouse.y, img_x, img_y)}
-             on:load={loadImg}
+            style="width: {new_width}px;"
+            bind:this={img}
+            src={img_src}
+            alt="Annotated file for file id {selected}"
+            on:dblclick={(mouse) => console.log(mouse.offsetX, mouse.offsetY, mouse.x, mouse.y, img?.x, img?.y)}
+            on:load={loadImg}
          />
         {#each $fileItems as fileItem}
-            <div class="test" style="left: {img_x + fileItem.x}px; top: {img_y + fileItem.y}px">0</div>
+            <button class="box" style="
+                      position: absolute;
+                      left: {img?.x + fileItem.top * new_width / original_width}px;
+                      top: {img?.y + fileItem.left * new_width / original_width}px;
+                      width: {fileItem.width * new_width / original_width}px;
+                      height: {fileItem.height * new_width / original_width}px;
+                      border-color: #ff0000;
+                      background-color: transparent;
+                      border-width: 3px;
+                      color: #000000"
+            >
+            </button>
+            <div class="item"
+                 style="
+                      position: absolute;
+                      color: #ff0000;
+                      transform: translate(-50%, -50%);
+                      left: {img?.x + fileItem.x * new_width / original_width}px;
+                      top: {img?.y + fileItem.y * new_width / original_width}px"
+            >&#9733;</div>
         {/each}
     </div>
 </main>
 
 <style>
-    .annotated-file {
-    }
-    .test {
-        position: absolute;
-        background-color: #ff0000;
-    }
 </style>
